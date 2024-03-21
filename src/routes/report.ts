8b6016d7225f8c32438b21325c284e90bbcf0d28
CheBarash <chebarashek@gmail.com>
2024-03-22 01:54:47 +0500

    Update report.ts

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -13,11 +13,11 @@ const report =
       .get(
         `https://api.telegram.org/bot${BOT}/sendMessage?chat_id=${ADMIN}&text=${encodeURIComponent(
           bug
-        )}`,
-        (resp) =>
-          resp.on("end", () => res.json({ message: `Report sent! Thank You` }))
+        )}`
       )
       .on("error", (err) => console.log("Error: " + err.message));
+
+    res.json({ message: `Report sent! Thank You` });
   };
 
 export default report;
