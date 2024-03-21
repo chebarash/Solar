644989e139a3bbc6f7d146c6179c07c1597f5d85
CheBarash <chebarashek@gmail.com>
2024-03-22 02:58:15 +0500

    Bug fix

--- src/routes/icon.ts
+++ src/routes/icon.ts
@@ -13,7 +13,11 @@ const icon = async (
 
     res
       .setHeader("content-type", "image/svg+xml")
-      .send(color ? icon.replace(/#1C274C/gi, color) : icon);
+      .send(
+        `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">${
+          color ? icon.replace(/#1C274C/gi, color) : icon
+        }</svg>`
+      );
   } catch (e) {
     res.status(400).json({ message: `No such icon` });
     console.log(e);
